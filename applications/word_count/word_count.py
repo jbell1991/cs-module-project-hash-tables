f92eaea86896dc5cb441f9a49a29e9f2fe8e2c8c
jbell1991 <joseph.louis.bell@gmail.com>
2020-07-08 16:51:21 -0400

    finished word count

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,6 +1,26 @@
 def word_count(s):
-    # Your code here
+    # take in sentence as the input
+    counts = {}
+    # convert string to lowercase
+    s = s.lower()
+    # get rid of punctuation
+    punctuations = '''":;,.-+=/\\|[]{}()*^&'''
+    for char in s:
+        if char in punctuations:
+            s = s.replace(char, '')
+    # iterate through each word in the sentence using split
+    for word in s.split():
+        # if it's the first time seeing the key
+        if word not in counts:
+            # set the value to 1
+            counts[word] = 1
+        # if it's a duplicate key
+        else:
+            # add 1 to the value 
+            counts[word] += 1
 
+    # return dictionary with each word as the key and count of each word as the value
+    return counts
 
 
 if __name__ == "__main__":
